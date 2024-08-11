2d5187a8ddbb0223d5c71a80f37013ab43e0b56d
Mark Wang <armstrong.wang2000@gmail.com>
2024-08-10 21:53:58 -0600

    Remvoe unnecessary interface extends

--- src/main/java/com/wangf/spring/repository/jdbc/BookJDBCRepository.java
+++ src/main/java/com/wangf/spring/repository/jdbc/BookJDBCRepository.java
@@ -1,12 +1,11 @@
 package com.wangf.spring.repository.jdbc;
 
 import com.wangf.spring.repository.common.BookCachingRepository;
-import com.wangf.spring.repository.common.BookRepositoryCustom;
 import io.micrometer.common.lang.NonNullApi;
 import org.springframework.context.annotation.Profile;
 
 @Profile("jdbc")
 @NonNullApi
-public interface BookJDBCRepository extends BookCachingRepository, BookRepositoryCustom {
+public interface BookJDBCRepository extends BookCachingRepository {
 
 }
