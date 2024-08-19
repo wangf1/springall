092ebb109bf0d5a7d58256251ef80407fb68a02c
Mark Wang <armstrong.wang2000@gmail.com>
2024-08-18 22:01:12 -0600

    TDD part 3: Mock Repository to make test pass.

--- microservice-product/src/test/java/com/wangf/spring/product/tdd/TddControllerTest.java
+++ microservice-product/src/test/java/com/wangf/spring/product/tdd/TddControllerTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -19,8 +23,17 @@ public class TddControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private TddRepository tddRepository;
+
     @Test
     void shouldReturnAllCus() throws Exception {
+
+        doReturn(List.of(
+                new Customer(1L, "Bob"),
+                new Customer(2L, "Alice"))).
+                when(tddRepository).findAll();
+
         mockMvc.perform(get("/customers"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("@.[0].id").value(1L))
